6f7f7697c776396384839503c71f53d0c0faf406
Joshua Hawxwell <m@hawx.me>
2013-03-11 19:28:24 +0000

    Document hadfield.go

--- hadfield.go
+++ hadfield.go
@@ -1,3 +1,6 @@
+// Package hadfield implements a basic subcommand system to complement the
+// standard libraries 'flag' package. To use, in your 'main' function you must
+// call the Run function with a list of Commands and Templates.
 package hadfield
 
 import (
@@ -7,6 +10,9 @@ import (
 	"log"
 )
 
+// Run executes the correct subcommand. It delegates the subcommand 'help' to
+// the help function below. If it can not find a subcommand with the name given
+// it exits with an error.
 func Run(cmds Commands, templates Templates) {
 	flag.Usage = func() { usage(templates, cmds) }
 	flag.Parse()
@@ -33,11 +39,15 @@ func Run(cmds Commands, templates Templates) {
 	os.Exit(2)
 }
 
+// usage prints a usage message and then exits.
 func usage(templates Templates, cmds Commands) {
 	templates.Usage.Render(os.Stderr, cmds)
 	os.Exit(0)
 }
 
+// help controls the "help" pseudo-command. It will print the usage message if
+// given an empty list of arguments. It prints the associated help text if given
+// a signle argument. And otherwise exists with an error.
 func help(templates Templates, cmds Commands, args []string) {
 	if len(args) == 0 {
 		templates.Usage.Render(os.Stdout, cmds)
@@ -45,6 +55,7 @@ func help(templates Templates, cmds Commands, args []string) {
 	}
 	if len(args) != 1 {
 		fmt.Fprintf(os.Stderr, "help given too many arguments\n")
+		os.Exit(2)
 	}
 
 	arg := args[0]
